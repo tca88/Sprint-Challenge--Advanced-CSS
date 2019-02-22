a7a33795b9a42e76929ff46cd596ce2b422fe82f
tca88 <trishnaamin@gmail.com>
2019-02-22 16:17:18 -0500

    Add jquery to form to fixe UI of inputting information. Add index.js file for this and link to it from contact.html.

---
+++ index.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {$( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+       $(this).addClass('not-empty');
+    } else {
+       $(this).removeClass('not-empty');
+    }
+  })});
